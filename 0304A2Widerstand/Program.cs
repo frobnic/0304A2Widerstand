36883a14ddf20bf4fea214133be3ada9750e49b1
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 10:34:02 +0200

    code inserted

--- 0304A2Widerstand/Program.cs
+++ 0304A2Widerstand/Program.cs
@@ -10,6 +10,53 @@ namespace _0304A2Widerstand
     {
         static void Main(string[] args)
         {
+            double R, l, d, k;
+            char m;
+
+            Console.Write("Bitte erfassen sie den Durchmesser in mm : ");
+            d = double.Parse(Console.ReadLine());
+            Console.Write("Bitte erfassen sie die Länge in m        : ");
+            l = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bitte erfassen sie das Material");
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("!(S)ilber   !");
+            Console.WriteLine("!(K)upfer   !");
+            Console.WriteLine("!(A)luminium!");
+            Console.WriteLine("!(M)essing  !");
+            Console.WriteLine("+-----------+");
+
+            m = char.ToLower(Console.ReadKey().KeyChar);
+            
+            if (m != 's' && m != 'k' && m != 'a' && m != 'm')
+                Console.WriteLine("Nur Eingabe von s, k, a oder m erlaubt!");
+            else
+            {
+                switch (m)
+                {
+                    case 's':
+                        k = 60.6;
+                        break;
+
+                    case 'k':
+                        k = 56.8;
+                        break;
+
+                    case 'a':
+                        k = 36.0;
+                        break;
+                        
+                    default:
+                        k = 13.3;
+                        break;
+                }
+
+                R = l / (k * ((Math.PI * Math.Pow(d, 2)) / 4));
+
+                Console.WriteLine("Der Widerstand beträgt {0:F4} Ohm", R);
+
+            }
+
         }
     }
 }
